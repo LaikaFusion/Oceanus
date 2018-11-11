5c9c1108d76c16b497b675da6deb4ab41bacd4ee
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-11 12:06:45 -0500

    set up port dynamically

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(3000, () => console.log('listening on port 3000...'));
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
