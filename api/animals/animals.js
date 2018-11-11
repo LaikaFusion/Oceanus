73542abc7ef01df32ab75e27eee79d72ef395896
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-11 14:19:40 -0500

    got main object with group names

---
+++ api/animals/animals.js
@@ -0,0 +1,53 @@
+
+const animals = [
+    {
+        status: 'success',
+        brachiopods: {
+            groupName: 'brachiopods'
+        },
+        coralsAndSeaAnemones: {
+            groupName: 'corals and sea anemones'
+        },
+        crustaceans: {
+            groupName: 'crustaceans'
+        },
+        fishes: {
+            groupName: 'fishes'
+        },
+        hydrozoans: {
+            groupName: 'hydrozoans'
+        },
+        jellyfishAndRelatedSpecies: {
+            groupName: 'jellyfish and related species'
+        },
+        lancelets: {
+            groupName: 'lancelets'
+        },
+        mammals: {
+            groupName: 'mammals'
+        },
+        molluscs: {
+            groupName: 'molluscs'
+        },
+        reptiles: {
+            groupName: 'reptiles'
+        },
+        seaSpiders: {
+            groupName: 'seaSpiders'
+        },
+        sponges: {
+            groupName: 'sponges'
+        },
+        starfishAndOtherEchinoderms: {
+            groupName: 'starfish and other echinoderms'
+        },
+        turnicates: {
+            groupName: 'turnicates'
+        },
+        worms: {
+            groupName: 'worms'
+        }
+    }
+];
+
+module.exports = animals;
--- api/routes/animals.js
+++ api/routes/animals.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const animals = require('../animals/animals');
+
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling get request to /animals'
-    });
+    res.status(200).json(animals);
 });
 
 router.get('/:species', (req, res, next) => {
