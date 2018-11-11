d564cc8ff241aed3bb8cf6e119ac57ebc620917a
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-11 13:57:09 -0500

    added error handling

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const animalRoutes = require('./api/routes/animals');
 
+app.use(morgan('dev'));
+
 app.use('/api/animals', animalRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('404: Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
