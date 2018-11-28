9c83d6540fd7d452607d3dd4315be6665674c1aa
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-27 23:48:32 -0500

    finished the list of opossum shrimp

--- api/animals/crustaceans/opossumShrimp/index.js
+++ api/animals/crustaceans/opossumShrimp/index.js
@@ -31,6 +31,64 @@ const animals = {
        remarks: `Amblyops kempi can be distinguished from Amblyops abbreviata by: the presence of spinules all over the eyeplates, the particularly large spine on the basal joint of the antenna, the long slender spines on the endopod of the uropod and,  the shape and armature of the telson. Amblyops kempi is evidently a deeper water form than Amblyops abbreviata, but there is still overlap in the distributions of the two species. Amblyops kempi lives close to the sea bottom making only short occasional excursions into the lower levels of water just above the floor.`,
        distributionInTheNorthSea: `Probably rarae in the northern North Sea.`,
        worldDistribution: `North Atlantic, 44 - 64°N; shelf. Coastal to bathyal; e.g. west coast of Ireland, west of the Hebrides, Faeroes.` 
+    },
+    amblyopsoidesObtusa: {
+        name: `Amblyopsoides obtusa`,
+        description: `Antennal scale with apical lobe very long, occupying nearly 1/2 of its length. Telson trapezoidal; apex 1/3 as broad as at base, with 2 pairs of long spines, a pair of minute spines and a pair of median plumose setae; lateral margin with dense row of spines.`
+    },
+    anchialinaAgilis: {
+        name: `Anchialina agilis`,
+        description: `Generic features: General form compact and robust. Carpus large. Propodus divided into two to three subsegments. Posterior margin of carapace transverse. Integument covered with very small bristles or tiny spinules which are particularly noticeable on the dorsal surface of the telson and on the large basal segments of the appendages. Pleural plates of first abdominal somite only slightly produced. Eyes large with thick short eyestalks. Telson very long with small apical cleft. Male pleopods biramous well developed, natatory, third pair with exopod slightly elongated. Spines on outer margin of exopod of uropod small; large spines on distal portions of inner margin of endopod. Species: Carapace large, covering the whole of the thorax and more than half the first abdominal somite in dorsal view, widening considerably posteriorly, posterior margin straight and transverse. Rostrum large, narrowly biangular, covering part of the bases of the antennular peduncles, the anterior tip is usually bent downward between the antennules and so appears to be shorter than it really is. Antennular peduncle in both sexes short and thick, first segment only slightly longer than the eyes, second segment very short and hollowed out at its anterior- end forming a kind of basin in which the third segment rests; inner margins of second and third segments in the male armed with a few long plumose setae. Antennal scale very small, extending only slightly beyond the first segment of the antennular peduncle, outer margin naked, terminating in a small spine beyond which the apex extends considerably, small distal suture present. On the inner distal corner of the sympod there is a large serrated spine projecting forward. Eyes thick, cornea occupying about a third of the whole eye, golden brown in colour. Labrum produced anteriorly into a long spine serrated along the margins. First thoracic limb larger in the male than in the female, without lobes, dactylus small, bearing a long, strong, curved nail, very large axe-shaped vibratory lamella in both sexes. Second thoracic limb short, basal segment large, carpus in the male enlarged irregularly along its inner and distal margins. Third thoracic limb basal joint large, carpus distinct, propodus divided at distal end by two or three transverse articulations, dactylus very small, without nail. distal end of limb in the male furnished with seven long, strapshaped appendages with oblique markings. Eighth thoracic limb, carpus long armed with setae and with three or four strong barbed spines at the distal end; propodus divided into four subsegments armed with strong barbed spines; genital appendix in the male large, armed with ovate, long setae. Pleopods in the male well developed, natatory, biramous (except the first pair in which the endopod is lacking), large oval pseudobranchial lamella borne at the base of endopod; third pair (not the fourth as described by Sars, 1877, P. 86) with exopod much longer than endopod and its distal segments and their setae modified; fourth pair with exopod longer than endopod but not modified. Pleopods in the female, first pair small, reduced to a simple stiliform segment. The remaining pairs are reduced to very delicate lamellae attached side by side to the posterior ventral margin of the somite and projecting backwards. The free distal margin is angular and bears a few plumose setae with two or three small spines. Telson very long, nearly half as long as the whole abdomen, three times as long as broad at the base; lateral margins straight, armed with 25-30 closely set, plumose spines, terminal ones much larger than the rest, cleft small, about one-seventh of the length of the telson, armed with a close row of about 25 teeth on each side. Apex of each lobe armed with a strong spine. Uropods with endopod longer than exopod, bowed inward at its distal end, inner margin armed with a graduated row of large plumose spines with small spines closely set between them. The two strong spines arming the distal end are very characteristic of the species, statocyst small; exopod with outer margin naked proximally, then armed with a close regular row of spines of which the distal ones are larger and may be slightly plumose.`,
+        size: `Adult males up to 9 mm, females 7-8 mm long.`,
+        depthRange: `Hyperbenthic, coastal to upper slope. A. agilis is found as a rule at or near to the bottom during daylight but it is capable of very rapid movement from one layer of water to another and can also be taken at the surface and in varying depths during the day. It has not been taken in depths of more than 85 m, but has been collected at the surface and in mid-water over 150 m in Irish waters. It has been most frequently taken at the surface at midnight when it occurs in large numbers.`,
+        remarks: `This species can easily be recognised by the robust compact form and large carapace with transverse posterior margin; the large serrated spine at the base of the antennal peduncle; the very small antennal scale; the swollen carpal segment in the second thoracic leg in the male; the peculiar appendages borne at the distal end of the endopod of the third thoracic limb in the male, the small epimeral plates of the first abdominal somite and the very unusual modified pleopods of the second to the fifth somites in the female; the armature and proportions of the uropods with the inward curve of the endopod and the large telson with its small cleft.`,
+        distributionInTheNorthSea: `Eastern North Sea, maybe in the SW North Sea entering from the E English Channel.`,
+        worldDistribution: `E North Atlantic: <26 - 56°N; Mediterranean.`
+    },
+    anchialinaTruncata: {
+        name: `Anchialina truncata`,
+        description: `Rostrum abruptly truncate at tip. Exopod of 3rd pleopod with short and straight setae on several distal segments. Probably a coastal species.`
+    },
+    anchialinaTypica: {
+        name: `Anchialina typica`,
+        description: `Frontal margin of carapace produced into short trapezoidal plate with rounded angles, plate bent strongly downwards, as a result of which true anterior margin is invisible from above. Exopod of male 3rd pleopod with L-shaped, naked setae on several middle segments.`
+    },
+    arachnomysisLeuckarti: {
+        name: `Arachnomysis leuckarti`,
+        description: `Anterior margin of carapace and posterior margin of abdominal somites with acute spines. Antenna without scale. Eyes conical; number of facets across the largest diameter of eye 10-14. Telson very small; lateral margins smooth; apex narrow, with a pair of very small spines.`
+    },
+    arachnomysisMegalops: {
+        name: `Arachnomysis megalops`,
+        description: `Anterior margin of carapace and posterior margin of abdominal somites with acute spines. Antenna without scale. Eyes conical; number of facets across the largest diameter of eye 18-25. Telson very small; lateral margins smooth; apex narrow, with a pair of very small spines.`
+    },
+    arthromysisMagellanica: {
+        name: `Arthromysis magellanica`,
+        description: `Antennal scale long, setose all around. Pleopods: 3rd pleopod with multi-jointed exopod and unjointed endopod; 4th pleopod with extremely long, multi-jointed exopod which is armed with very long modified setae on distal segments, and unjointed endopod; 5th pleopod with both rami multi-jointed Lateral margin of telson with row of subequal spines.`
+    },
+    boreomysisAcuminata: {
+        name: `Boreomysis acuminata`,
+        description: `Carapace with rostrum with narrow, very sharply pointed apex. Eyestalk expanded distally, forming large triangular projection; cornea small, restricted to distal outer area of eyestalk.`
+    },
+    boreomysisArctica: {
+        name: `Boreomysis arctica`,
+        description: `Generic features: Statocyst in the uropods small. Pleopods of female reduced, rudimentary; of male variable. Exopod of uropod with proximal portion of outer margin naked, marked distally by one or two spines and an incipient articulation. Marsupium with seven pairs of oostegites. Telson cleft. Species: Body less robust than in Boreomysis tridens. Carapace with rostral plate triangular, pointed; with the anterior margin produced into a median acutely spiniform rostrum which extends beyond the eyes and almost to the end of the first segment of the antennular peduncle; antero-lateral corners of the carapace acutely produced. Eyes large, pyriform, cornea wider than the eyestalk. Eye pigment, golden to red-brown. Antennal scale less than twice as long as the antennular peduncle, five times as long as broad, outer margin straight, entire, terminating in a strong spine beyond which the apex of the scale is not produced. Uropod with endopod slightly longer than the telson and having two spines on the proximal end of the lower inner margin; exopod one-quarter longer and considerably broader, than the endopod with truncate apex; proximal unarmed part of the outer margin equal to one-quarter of the entire length of the exopod and terminated by two spines from which an incomplete suture runs obliquely across the exopod; statocyst small. Telson longer than last abdominal somite, three times as long as broad at base; apex cleft to one-fourth of the entire length of the telson, margins of cleft slightly diverging, armed with a continuous row of saw-like teeth; no proximal dilation to the cleft; no plumose setae.`,
+        size: `Length of adults of both sexes 27 mm.`,
+        depthRange: `Benthopelagic. Recorded depths range from 380-2000 m.`,
+        remarks: `Boreomysis arctica is closely related to Boreomysis tridens and the only obvious difference between them is in the form of the rostrum. Minor differences can be detected in the proportions of the antennal scale, 5 times as long as broad in Boreomysis arctic as against 4 times in Boreomysis tridens, in the relatively shorter unarmed portion of the outer margin of the exopod of the uropod, one-quarter of the entire length in Boreomysis arctica as against one-third in Boreomysis tridens.`,
+        distributionInTheNorthSea: `Northern North Sea, off W Norway, Skagerrak, Oslofjorden.`,
+        worldDistribution: `E North Atlantic: 28-79°N, Mediterranean, North Pacific; shelf, slope. Distribution includes the North Atlantic quite similar to that of Boreomysis tridens , namely, in boreal waters extending southwards on the eastern slope from Norway to the Bay of Biscay and on the American side from West Greenland to the coasts of New England. In addition, however, it is known from the Mediterranean and from the North Pacific in the Bering Sea, off Japan and off the Coasts of California. Its geographical range (E North Atlantic 28-79°N, Mediterranean, North Pacific; shelf, slope) is thus wider than that of Boreomysis tridens.`
+    },
+    boreomysisAtlantica: {
+        name: `Boreomysis atlantica`,
+        description: `Anterior margin of carapace broadly rounded with median small triangular projection. Eyes with large cornea, somewhat flattened; eyestalks with small papilla on dorsal surface. `
+    },
+    boreomysisBispinosa: {
+        name: `Boreomysis bispinosa`,
+        description: `Anterior margin of carapace rounded, with extremely short upturned projection. Endopod of 2nd pereopod with 2 spines on carpopropodus.`
+    },
+    boreomysisIlligi: {
+        name: `Boreomysis illigi`,
+        description: `Carapace with triangular rostrum, apex acutely pointed, lateral margins convex. Eyes large; papilla very long, extending forward beyond anterior margin of cornea. `
     }
 };
 
