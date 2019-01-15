d79e7be0b11b535571eebdd1324bfe646f3ead54
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-14 21:49:32 -0500

    added images to barnacles in crustaceans

--- api/animals/crustaceans/barnacles/index.js
+++ api/animals/crustaceans/barnacles/index.js
@@ -1,10 +1,13 @@
+const baseURL = require('../../../baseURL');
+
 const animals = {
     ribbedBarnacle: {
         id: 1,
         name: `Ribbed barnacle (Tetraclita stalactifera)`,
         description: `The ribbed barnacle chosen here as a representative of several genera of barnacle-like animals. They all live near the low water mark, although some may occur to great depths. The shape of the animals is conical to tubular, with calcareous plates. The color may vary from white to brown. Size: up to 3 cm.`,
         habitat: `Sessile, attached to any surface, wood or stone, man-made or natural. Depth: ranges from around the low water mark down to 40 m.`,
-        distribution: `Common`
+        distribution: `Common`,
+        img: `${baseURL}/img/crustaceans/img/barnacles/ribbedBarnacle/ribbed-barnacle.png`
     },
     smoothGooseNeckBarnacle: {
         id: 2,
@@ -12,7 +15,8 @@ const animals = {
         description: `Body covered by five translucent, white to bluish-white shell plates. The animal is attached to the substrate by a fleshy stalk that can reach a length of several centimeters. Size: up to 3 cm.`,
         habitat: `Usually attached to floating debris, rafts, buoys or ship bottoms.`,
         distribution: `Common`,
-        remarks: `Two more species of Lepas occur in the region, Lepas anserifa, with whiter shell plates which are radially grooved, and Lepas pectinata, with scaly or even spiny protrusions on the plates.`
+        remarks: `Two more species of Lepas occur in the region, Lepas anserifa, with whiter shell plates which are radially grooved, and Lepas pectinata, with scaly or even spiny protrusions on the plates.`,
+        img: `${baseURL}/img/crustaceans/img/barnacles/smoothGooseNeckBarnacle/smooth-goose-neck-barnacle.png`
     }
 };
 
