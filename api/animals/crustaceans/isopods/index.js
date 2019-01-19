62355523439c59d41e70924e5599a1c6c1d1e77c
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-18 21:16:24 -0500

    added isopods images to crustaceans folder

--- api/animals/crustaceans/isopods/index.js
+++ api/animals/crustaceans/isopods/index.js
@@ -1,3 +1,5 @@
+const baseURL = require('../../../baseURL');
+
 const animals = {
     soldierfishIsopod: {
         id: 1,
@@ -7,21 +9,24 @@ const animals = {
         size: `length up to 3 cm.`,
         habitat: `Parasitic. Most infected are Blackbar Soldierfishes Myripristis jacobus.`,
         distribution: `Occasional all throughout the Caribbean.`,
-        remarks: `Many more species in this genus occur, but identification requires expert knowledge.`
+        remarks: `Many more species in this genus occur, but identification requires expert knowledge.`,
+        img: `${baseURL}/img/crustaceans/img/isopods/soldierfishIsopod/soldierfish-isopod.jpg`
     },
     ancyroniscusBonnieri: {
         id: 2,
         name: `Ancyroniscus bonnieri`,
         description: `Female a bilobed sac, with few distinct segments and lacking pereiopods. Male (cryptoniscus) antenna 1 with the basal article lamellar and often serrated, pereiopods 3 to 7 slender with setiform dactyls; pleopods and uropods biramous. Microniscus stage and its host unknown. Initially infection takes place through the protandrous males, which attach to the gut of both males and females of the host. Further developement of the parasite only occurs if the host is female. One male developes as a female, it protrudes the head and anterior pleon into the brood pouch of the host. Infected host females lose their own eggs and the eggs of the parasite are visible through the host cuticle. Release of embryos is facilitated by a ventral split in the external part of the pereion of the parasite; epicardian larvae escape via the host brood pouch.`,
         habitat: `Parasitic on the sphaeromid isopod Dynamene bidentata .`,
-        distribution: `A southern species.`
+        distribution: `A southern species.`,
+        img: `${baseURL}/img/crustaceans/img/isopods/ancyroniscusBonnieri/ancyroniscus-bonnieri.png`
     },
     anthuraGracilis: {
         id: 3,
         name: `Anthura gracilis`,
         description: `Body long and narrow, subcylindrical. Eyes large, particularly in males. Antenna 1 of female small, those of male with the flagellum elongate and densely clothed with filamentous setae. Pereional segments longer than wide. Pleon segments of females at most only slightly distinct, those of males more distinct; uropodal exopods arching dorsally and medially over the telson. Telson subquadrate, widest posteriorly. Females up to 11 mm body length, males smaller, up to 4 mm.`,
         habitat: `In fully marine situations, mainly sublittoral, but occasionally between tidemarks in crevice-like situations such as among Laminaria holdfasts, and in empty tubes of polychaetes, such as Pomatoceros , Spirorbis and Sabellaria . `,
-        distribution: `A southern species, recorded from Millport, the Isle of Man, South Wales, southwest England and the Mediterranean.`
+        distribution: `A southern species, recorded from Millport, the Isle of Man, South Wales, southwest England and the Mediterranean.`,
+        img: `${baseURL}/img/crustaceans/img/isopods/anthuraGracilis/anthura-gracilis.jpg`
     },
     arcturellaDilatata: {
         id: 4,
@@ -31,7 +36,8 @@ const animals = {
         colour: `brownish, with darker patches.`,
         habitat: `Sublittoral, down to 360 metres.`,
         distribution: `Norway, east to the Kattegat, between Orkney and Shetlands and in the Mediterranean`,
-        remarks: `One other species of Arcturella is recorded off the west coasts of the British Isles: Arcturella damnoniensis (Stebbing, 1874). This species has a single large process in the centre of the middle pereional segment.`
+        remarks: `One other species of Arcturella is recorded off the west coasts of the British Isles: Arcturella damnoniensis (Stebbing, 1874). This species has a single large process in the centre of the middle pereional segment.`,
+        img: `${baseURL}/img/crustaceans/img/isopods/arcturellaDilatata/arcturella-dilatata.gif`
     },
     astacillaLongicornis: {
         id: 5,
@@ -39,7 +45,8 @@ const animals = {
         description: `Body extremely slender and subcylindrical. The segment bearing pereiopod 1 fused with the cephalon. Middle segment (4) of the pereion about twice the length of anterior segments and the cephalon; in female the surface is covered with small tubercles, two of which are conspicuous anteriorly on dorsal surface; in male the surface is smooth and cylindrical. Pereiopods 1 to 4 elongate, very densely setose and directed toward the mouth; pereiopods 5 to 7 short, stout and adapted for clinging.`,
         size: `Female up to 30 mm body length; males up to 10 mm.`,
         habitat: `There are some intertidal records, presumably of specimens washed up from below tidemarks.`,
-        distribution: 'Ranging from northern Norway, east to the Kattegat and southwards to Portugal.'
+        distribution: 'Ranging from northern Norway, east to the Kattegat and southwards to Portugal.',
+        img: `${baseURL}/img/crustaceans/img/isopods/astacillaLongicornis/astacilla-longicornis.jpg`
     },
     athelgesPaguri: {
         id: 6,
@@ -47,7 +54,8 @@ const animals = {
         description: `Fully grown female markedly asymmetrical. Cephalon small and withdrawn into the pereion in which the segments may be distinct only in the middle region, if at all. Pereion with seven pairs of pereiopods and five pairs of oostegites; the oostegites very large with the anterior pairs elaborately folded and projecting beyond the cephalon. Pleotelson narrowing abruptly, with only four distinct segments, bearing four pairs of stalked biramous pleopods with each ramus round and flat. Uropods barely visible, as two very small tubercles on the club-like posterior part of the pleon. Male pleotelson without segments.`,
         size: `Females up to 11 mm body length, males 4 mm.`,
         habitat: `It occurs intertidally on Pagurus bernhardus , and in deeper waters on Anapagurus laevis , usually attached to the abdomen but occasionally in the gill chamber. `,
-        distribution: `The species appears to be distributed around all coasts of the British Isles and Ireland.`
+        distribution: `The species appears to be distributed around all coasts of the British Isles and Ireland.`,
+        img: `${baseURL}/img/crustaceans/img/isopods/athelgesPaguri/athelges-paguri.png`
     }
 };
 
