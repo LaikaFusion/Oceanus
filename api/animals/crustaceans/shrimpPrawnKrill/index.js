3d061a2878b83ff03607c57487a1289211a947d0
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-23 21:07:52 -0500

    added a few items to shrimp and krill

--- api/animals/crustaceans/shrimpPrawnKrill/index.js
+++ api/animals/crustaceans/shrimpPrawnKrill/index.js
@@ -4,7 +4,8 @@ const animals = {
     bandedCoralShrimp: {
         id: 1,
         name: `Banded coral shrimp (Stenopus hispidus)`,
-        description: `Like all members of this group of shrimps, the Stenopodids, the third pair of walking legs is of different shape and bears claws. This cleaning shrimp is easily recognized by the bands of red and white. The bands of red can be purple bordered. The bands extend onto the claws. Some parts of the animal and its walking legs can be translucent. The abdomen, carapace and third pair of legs are spiny. Size: up to about 5 cm.`,
+        description: `Like all members of this group of shrimps, the Stenopodids, the third pair of walking legs is of different shape and bears claws. This cleaning shrimp is easily recognized by the bands of red and white. The bands of red can be purple bordered. The bands extend onto the claws. Some parts of the animal and its walking legs can be translucent. The abdomen, carapace and third pair of legs are spiny.`,
+        size: `up to about 5 cm.`,
         habitat: `Inhabits the coral reefs near openings or recesses. The shrimp waves its antennae to attract fish. It will feed on the parasites it removes from the fish's skin. `,
         habitatDepths: `ranges from 2 m down to 40 m.`,
         distribution: `All throughout the Caribbean.`,
@@ -14,7 +15,8 @@ const animals = {
     pedersonCleaningShrimp: {
         id: 2,
         name: `Pederson cleaning shrimp (Periclimenes pedersoni)`,
-        description: `A small shrimp, translucent with bluish or lavender spots on the tail, the abdomen and legs. Size: up to 3 cm.`,
+        description: `A small shrimp, translucent with bluish or lavender spots on the tail, the abdomen and legs.`,
+        size: `up to 3 cm.`,
         habitat: `It lives in association with a variety of sea anemones, like Lebrunia danae, Bartholomea lucida, Bartholomea annulata and Condylactis gigantea. The shrimp waves its antennae to attract fish. It feeds on parasites that it removes from the fish's skin.`,
         habitatDepths: `ranges from 2 m down to 20 m.`,
         distribution: `All over the Caribbean.`,
@@ -41,6 +43,47 @@ const animals = {
         distribution: `Occurs in shallow reef areas al over the Caribbean.`,
         remarks: `When approached carefully with an extended hand, it may come out of its protection to clean it.`,
         img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/scarletStripedCleaningShrimp/scarlet-striped-cleaning-shrimp.jpg`
+    },
+    spottedCleanerShrimp: {
+        id: 5,
+        name: `Spotted cleaner shrimp (Periclimenes yucatanicus)`,
+        description: `This small shrimp looks like Periclimenes pedersoni, so it's body is transparent, but this shrimp is beautifully marked with colored spots and saddle-like markings. The claws and walking legs are striped with white, red and purple. Two pairs of white antennae with dark bands.`,
+        size: `up to 2.5 cm.`,
+        habitat: `It lives in association with a variety of sea anemones, in particular Condylactis gigantea but also Lebrunia danae, Bartholomea lucida, Bartholomea annulata. The shrimp waves its antennae to attract fish. It will feed on the parasites it removes from the fish's skin.`,
+        habitatDepths: `ranges from 2 m down to 20 m.`,
+        distribution: `All over the Caribbean.`,
+        remarks: `When approached carefully with an extended hand, it may come out of its protection to clean it.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/spottedCleanerShrimp/spotted-cleaner-shrimp.jpg`
+    },
+    squatAnemoneShrimp: {
+        id: 6,
+        name: `Squat anemone shrimp (Thor amboinensis)`,
+        description: `The body color is a light brownish gray, with three cream to white saddles, one over the carapace, and several cream to white spots. The spots and saddles may have dark blue outlines. Small antennae. The eyes have the same color as the saddles and spots, and appear therefore very large.`,
+        size: `ca. 1 cm.`,
+        habitat: `Typical anemone shrimp, often associates with Giant anemone Condylactis gigantea, Sun anemone Stichodactyla helianthus and Elegant anemone Actinoporus elegans.`,
+        habitatDepths: `ranges from 2 m down to 20 m.`,
+        distribution: `All over the Caribbean.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/squatAnemoneShrimp/squat-anemone-shrimp.jpg`
+    },
+    athanasNitescens: {
+        id: 7,
+        name: `Athanas nitescens`,
+        description: `Rostrum straight, unarmed, normally extending little beyond distal end of basal segment of antennular peduncle, apex acutely pointed. Carapace with anterior projections which partly cover the eyes in dorsal view, the projections bearing two lateral spines plus small dorsal lobe. Stylocerite narrow, acutely pointed, 0.66-0.75 x length of scaphocerite, slightly longer in large specimen. Scaphocerite equal in length to antennular peduncle, apical spine stout, exceeding lamellar portion. Third maxilliped about equal in length to scaphocerite; epipod and exopod present. Mandible with incisor and molar process, and two-segmented palp. Pereiopods 1-3 with epipods, setobranch on pereiopod 4. Pereiopod 1 with reduced exopod. First pereiopods larger than second pair, often asymmetrical in male. Carpus of pereiopod 2 with five segments. Pleonite 6 with articulated process at base of uropods. Telson with two pairs of lateral spines.`,
+        size: `Length up to 20 mm.`,
+        color: `Variable. Often green, blue or red-brown, frequently with a longitudinal white dorso-median stripe extending the full length of the animal. May also be almost transparent, speckled with red chromatophores.`,
+        habitat: `Down to about 65 metres. Hyperbenthic species, may swim up at night, but mostly confined to the nepheloid layer. Occasionally in rock-pools. May occur under large stones especially where the substrate is gravelly. Often occur in groups of adults and juveniles. Ovigerous females occur from May to August-September.`,
+        distribution: `All North Sea. Ranges from S and W Norway and W coast of Sweden southwards to the Cape Verde Islands and into the Mediterranean and Black Sea.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/athanasNitescens/athanas-nitescens.jpg`
+    },
+    bentheuphausiaAmblyops: {
+        name: `Bentheuphausia amblyops`,
+        description: `Fresh specimens have a deep red colour; the gills are white and prominent, especially those on the thoracic legs 6-8. There are no luminescent organs. The eye is very small, oval, and reddish-brown, with ommatidia imperfectly developed; there are no photophores present. The ratio of the eye diameter and the carapace length is about 0.08. [B.amblyops-eye-ph]. There is a small, knob-like projection (tubercle) located on the upper anterior edge of the eyestalk adjacent to the eye. The peduncle of the first antenna appears stout or massive. The first and second segments are particularly high, with the dorsal portions of each extending forward as plates [B.amblyops lappet ] that appear to reinforce successive joints of the peduncle; the two distal segments are somewhat thicker in male than female. The basal part of the upper flagellum of the first antenna is heavily setose in the male and less so in the female. Rostrum short, scarcely an acute triangle extending forward to the mid-point of the eye [B.amblyops-ph]. Carapace without lateral denticles in the adult. In young specimens of about 8-17 mm, the postero-lateral margin is serrated. Thoracic legs all well developed; however, legs 8, 7, and 6 are notably smaller than the more anterior legs. Abdomen is uniform and symmetrical, without keels or spines. The sixth segment is short, 1.3 times longer than its maximum vertical dimension. [B.amblyops-drawing]. Photophores absent. Petasma: the endopod of the first pleopod is not modified as a complex petasma in the male, but the basipod carries 1-6 stout spines and the endopod bears a small lobe [B.amblyops petasma ].`,
+        size: `Adults are 25-50 mm.`,
+        habitatDepths: `B. amblyops is a bathypelagic species living in deep water, probably from 500 to >2000 m. No day-night difference is evident in the vertical distribution.`,
+        distribution: `This deep sea species is absent from the shallow North Sea. However, regarding the presence of B. amblyops in the adjacent waters of the NE Atlantic, the species is likely to occur in the northern North Sea, in the vicinity of the Orkney and Shetland Islands, as well as in the Norwegian Deep. Indeed, B. amblyops is reported from the entrance of the northern North Sea (S. Hay, pers. com.). B. amblyops is one of the most widely distributed euphausiids; found between 54°N and 54°S in the Pacific, between 64°N and 44°S in the Atlantic and in the tropical Indian Ocean basins.`,
+        remarks: `B. amblyops is the only species in the family Bentheuphausiidae.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/bentheuphausiaAmblyops/bentheuphausia-amblyops.jpg`
+
     }
 };
 
