a1893f3a8defe77cfe477678c51fff47ba91961b
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-23 23:26:14 -0500

    added more shrimp, prawn and krill to crustaceans

--- api/animals/crustaceans/shrimpPrawnKrill/index.js
+++ api/animals/crustaceans/shrimpPrawnKrill/index.js
@@ -76,14 +76,80 @@ const animals = {
         img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/athanasNitescens/athanas-nitescens.jpg`
     },
     bentheuphausiaAmblyops: {
+        id: 8,
         name: `Bentheuphausia amblyops`,
         description: `Fresh specimens have a deep red colour; the gills are white and prominent, especially those on the thoracic legs 6-8. There are no luminescent organs. The eye is very small, oval, and reddish-brown, with ommatidia imperfectly developed; there are no photophores present. The ratio of the eye diameter and the carapace length is about 0.08. [B.amblyops-eye-ph]. There is a small, knob-like projection (tubercle) located on the upper anterior edge of the eyestalk adjacent to the eye. The peduncle of the first antenna appears stout or massive. The first and second segments are particularly high, with the dorsal portions of each extending forward as plates [B.amblyops lappet ] that appear to reinforce successive joints of the peduncle; the two distal segments are somewhat thicker in male than female. The basal part of the upper flagellum of the first antenna is heavily setose in the male and less so in the female. Rostrum short, scarcely an acute triangle extending forward to the mid-point of the eye [B.amblyops-ph]. Carapace without lateral denticles in the adult. In young specimens of about 8-17 mm, the postero-lateral margin is serrated. Thoracic legs all well developed; however, legs 8, 7, and 6 are notably smaller than the more anterior legs. Abdomen is uniform and symmetrical, without keels or spines. The sixth segment is short, 1.3 times longer than its maximum vertical dimension. [B.amblyops-drawing]. Photophores absent. Petasma: the endopod of the first pleopod is not modified as a complex petasma in the male, but the basipod carries 1-6 stout spines and the endopod bears a small lobe [B.amblyops petasma ].`,
         size: `Adults are 25-50 mm.`,
         habitatDepths: `B. amblyops is a bathypelagic species living in deep water, probably from 500 to >2000 m. No day-night difference is evident in the vertical distribution.`,
         distribution: `This deep sea species is absent from the shallow North Sea. However, regarding the presence of B. amblyops in the adjacent waters of the NE Atlantic, the species is likely to occur in the northern North Sea, in the vicinity of the Orkney and Shetland Islands, as well as in the Norwegian Deep. Indeed, B. amblyops is reported from the entrance of the northern North Sea (S. Hay, pers. com.). B. amblyops is one of the most widely distributed euphausiids; found between 54°N and 54°S in the Pacific, between 64°N and 44°S in the Atlantic and in the tropical Indian Ocean basins.`,
         remarks: `B. amblyops is the only species in the family Bentheuphausiidae.`,
-        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/bentheuphausiaAmblyops/bentheuphausia-amblyops.jpg`
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/bentheuphausiaAmblyops/bentheuphausia-amblyops.png`
 
+    },
+    caridionGordoni: {
+        id: 9,
+        name: `Caridion gordoni`,
+        description: `Rostrum straight or very slightly down turned, about half the length of carapace and slightly longer than antennular peduncle; five to ten dorsal teeth (usually seven or eight), one ventral tooth (rarely two or three). Carapace with antennal spine. Stylocerite narrow, acutely pointed, 0.66 x length of antennular peduncle or slightly less. Scaphocerite outer border straight or slightly concave, apex acutely rounded with apical spine not exceeding lamellar portion. Antennal flagellum about 0.75 x length of body. Third maxilliped longer than scaphocerite by as much as last two joints; epipod and exopod present. Mandible with molar and incisor process, and three-segmented palp. Carpus of pereiopod 2 two- segmented, chelae long and slender. Pereiopods 1-4 with epipods, pereiopod 5 with reduced setobranch. Pleonite 4 usually without ventro-posterior tooth in female. Telson with two pairs of lateral spines.`,
+        size: `Length up to 27 mm.`,
+        color: `Carapace and pleon scattered with red chromatophores, darkest on rostrum, otherwise virtually transparent.`,
+        habitat: `Down to 10-1,000 metres, usually 20-500 metres. Normally found on rocky, sandy or gravelly substrata. Adults may swim up into the water column. Ovigerous females occur from March to October.`,
+        distribution: `All North Sea. Ranges from Iceland and Norway southwards into the Bay of Biscay. In the W Atlantic, this species is known from Newfoundland to Chesapeake Bay.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/caridionGordoni/caridion-gordoni.jpg`
+    },
+    caridionSteveni: {
+        id: 10,
+        name: `Caridion steveni`,
+        description: `Rostrum slightly downcurved, little shorter than antennular peduncle and slightly deeper than in Caridion gordoni; five to seven dorsal teeth, one ventral tooth. Carapace with antennal spine. Stylocerite narrow, acutely pointed, 0.5 -0.66 x length of antennular peduncle. Scaphocerite outer border straight, apex less acute than in Caridion gordoni, apical spine not exceeding lamellar portion. Antennal flagellum normally as long as body, sometimes a little shorter. Third maxilliped only slightly longer than scaphocerite; epipod and exopod present. Mandible with molar and incisor process, and three-segmented palp. Carpus of pereiopod 2 two-segmented, chelae long and slender. Pereiopods 1-4 with epipods, pereiopod 5 with reduced setobranch. Pleonite 4 with ventro-posterior tooth in both sexes. Telson with two pairs of lateral spines.`,
+        size: `Length up to 27 mm.`,
+        color: `Red, with diffuse yellow; scattered with red, small chromatophores.`,
+        habitat: `Down to about 30 metres. Usually a littoral species, occurring under stones and amongst algae at ELWS. It is not certain whether this species never swims up into the water column. Ovigerous females occur from January to May.`,
+        distribution: `Northern North Sea, British NE coasts, Skagerrak. Its full range is unknown, although its southern limit is probably SW England, since Bourdon (1965) records only larval stages at Roscoff and Zariquiey Alvarez (1968) makes no mention of it from Spanish shores. The northern limits are also unknown.`,
+        remarks: `Very similar to Caridion gordoni. Any Caridion found on the shore or in shallow water usually proves to be Caridion steveni.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/caridionSteveni/caridion-steveni.jpg`
+    },
+    crangonAllmani: {
+        id: 11,
+        name: `Crangon allmani`,
+        description: `Rostrum unarmed, about twice the length of the eye, triangular with rounded apex. Median line of carapace with anteriorly directed spine in anterior quarter; antennal, pterygostomian and hepatic spines present. Stylocerite acutely pointed, about 0.4 x length of antennular peduncle. Apical spine of scaphocerite exceeding the lamellar portion. Third maxilliped 1-1.2 x length of scaphocerite; exopod and arthrobranch present. Mandible with molar process only, teeth sharply pointed. Pereiopod 1 sub-chelate, stout. Pereiopod 2 extends to three-quarters of length of propus of pereiopod 1; dactyl of pereiopod 2 about a quarter the length of the propus. Pleonite 6 with a deep longitudinal dorsal groove and two parallel carinae. Endopods of pleopods 2-5 two-segmented, without appendix interna. Telson with two pairs of small lateral spines.`,
+        size: `Length up to 75 mm, usually less.`,
+        color: `Normally brownish-grey.`,
+        habitat: `Down to about 20-250 metres, occasionally down to 360 metres. Occurs on muddy or sandy substrates. Hyperbenthic species, may swim up at night, mostly confined to the nepheloid layer. Ovigerous females from December to July, although most have stopped breeding by May.`,
+        distribution: `All North Sea. Ranges from the White Sea southwards to the Bay of Biscay.`,
+        remarks: `This species closely resembles Crangon crangon, but is slightly slimmer and is easily distinguishable by the groove in pleonite 6.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/crangonAllmani/crangon-allmani.jpg`
+    },
+    crangonCrangon: {
+        id: 12,
+        name: `Crangon crangon`,
+        description: `Rostrum unarmed, half length of eye or slightly more, triangular with rounded apex. Median line of carapace with anteriorly directed spine in anterior quarter; antennal, pterygostomian and hepatic spines present. Stylocerite acutely pointed, about half length of antennular peduncle. Apical spine of scaphocerite exceeding lamellar portion. Third maxilliped equal in length to scaphocerite; exopod and arthrobranch present. Mandible with molar process only, teeth sharply pointed. Pereiopod 1 sub-chelate, stout. Pereiopod 2 extends to 0.75 x length of propus of pereiopod 1; dactyl of pereiopod 2 about 0.25 x length of propus. Pleonite 6 smooth dorsally, without groove(s) or carina(e). Endopods of pleopods 2-5 two-segmented, without appendix interna. Telson with two pairs of small lateral spines.`,
+        size: `Length up to 90 mm, usually less.`,
+        color: `A mottled grey or brown.`,
+        habitat: `Mid-shore-down to 150 metres. Found on sandy and muddy grounds. Penetrates the lower reaches of estuaries and is common in sandy bays. Hyperbenthic species, may swim up at night, mostly confined to the nepheloid layer. Ovigerous females occur at different times in different localities. There are often two or more spawning periods, and in some localities (e.g. Plymouth in S England), a few ovigerous females occur throughout the year. Elsewhere, breeding periods are restricted (e.g. February to June in the Bristol Channel).`,
+        distribution: `All North Sea. Ranges from the White Sea southwards into the Baltic and down to the Mediterranean.`,
+        remarks: `This is the common shrimp of sandy bays and beaches. Most authors note C. crangon to be a dioecious species, although some evidence suggests that it is a protandrous hermaphrodite. This is still a matter of conjecture (see Tiews, 1970). Important commercial fisheries for this species exist off the coasts of Germany, Holland, Belgium and France. It is also fished on a smaller scale from the east and west coasts of Britain.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/crangonCrangon/crangon-crangon.jpg`
+    },
+    dichelopandalusBonnieri: {
+        id: 13,
+        name: `Dichelopandalus bonnieri`,
+        description: `Rostrum longer than carapace, upcurved, with nine or ten (rarely eight) dorsal teeth, normally three of these behind posterior edge of orbit, although the third may be directly over the edge in juveniles; seven ventral teeth (rarely six or eight). Carapace with strong antennal spine and small pterygostomian spine. Stylocerite broadly rounded, shorter than eye. Scaphocerite outer border straight, often slightly concave in juveniles; apical spine equal to or slightly exceeding lamellar portion. Third maxilliped about 0.75 x length of scaphocerite; exopod and epipod present. Mandible with molar and incisor processes, and three-segmented palp with lateral lobe on proximal segment. Pereiopod 1 minutely chelate. Second pereiopods asymmetrical: pereiopod 2 right: carpus of five (rarely six or seven) segments; pereiopod 2 left: carpus of 38-45 segments, although normally about 30 in juveniles. Rarely are these segmental patterns completely reversed. Pereiopods 1-4 with arthrobranchs and epipods; pereiopod 5 with reduced setobranch. Telson with six to eight pairs of lateral spines.`,
+        size: `Length up to 160 mm, usually less than 100 mm.`,
+        color: `Pale reddish, often with bright red rostral tip.`,
+        habitat: `From 20 to 1000 metres deep. Ovigerous females from October to April.`,
+        distribution: `Northern North Sea. Ranges from Icelandic and Norwegian coasts southwards to the Bay of Biscay. Not found in Greenland waters.`,
+        remarks: `Unlike several other members of the Pandalidae, D. bonnieri is dioecious.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/dichelopandalusBonnieri/dichelopandalus-bonnieri.jpg`
+    },
+    eualusGaimardii: {
+        id: 14,
+        name: `Eualus gaimardii`,
+        description: `Rostrum curving upwards slightly, depth variable; twice length of antennular peduncle; five to seven dorsal teeth, three to five ventral teeth. Carapace with antennal and pterygostomian spines. Stylocerite acutely pointed, 0.66 x length of antennular peduncle or little more. Scaphocerite about twice the length of antennular peduncle, outer border straight becoming concave towards apex; apical spine not exceeding lamellar portion. Third maxilliped equal to or slightly shorter than scaphocerite; epipod and exopod present. Mandible with molar and incisor process, and two-segmented palp. Pereiopods 1 and 2 with epipods, reduced setobranch on pereiopod 3. Carpus of pereiopod 2 seven-segmented. Pleonite 4 with acute ventro-posterior spine. Telson with three to five pairs of lateral spines.`,
+        size: `Length up to 100 mm, usually 60-70 mm.`,
+        color: `Pale, translucent, with brownish-red markings on carapace and pleon.`,
+        habitat: `Down to about 10-200 metres. Hyperbenthic species, may swim up at night, mostly confined to the nepheloid layer. Ovigerous females occur from January to April, although the breeding period may be longer.`,
+        distribution: `Adults reported from southwestern and northern North Sea, British NE coasts. A circumpolar species, reaching its southern limits around northern Britain. It ranges from W Greenland, Jan Mayen, Spitzbergen, Franz Josef Land, N and W Norway, White Sea, Barents Sea, Siberia and Alaska southwards to Cape Cod (Massachusetts), the Firth of Forth and the Clyde Sea area.`,
+        remarks: `The rostrum of this species varies considerably in shape and armature ? illustrated by Dons (1915). Two forms of E. gaimardii were noted by Holthuis (1947). E. gaimardii forma gaimardii is a southern form with pleonite 3 smooth and rounded in both males and females. E. gaimardii forma belcheri (Bell, 1855) is a northern form, in which the posterior-median part of pleonite 3 in males bears a strong tubercle, which ends in a curved hook. The females have only a blunt, but distinct tubercle.`,
+        img: `${baseURL}/img/crustaceans/img/shrimpPrawnKrill/eualusGaimardii/eualus-gaimardii.jpg`
     }
 };
 
