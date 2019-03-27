c6794123ec1201621b7864c704e85baf25bacd94
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-27 12:11:17 -0400

    Tests are now working but an error appears

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,5 +8,11 @@ describe('Route Handlers', () => {
 
             expect(response.status).toBe(200);
         });
+
+        it('responds with an object', async () => {
+            const response = await request(app).get('/api/v1/animals');
+
+            expect(typeof response.body).toBe('object');
+        });
     });
 });
--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use((error, req, res, next) => {
     });
 });
 
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT);
+}
+
 app.listen(PORT, err => {
     console.log(`Server listening on port ${PORT}`);
 });
