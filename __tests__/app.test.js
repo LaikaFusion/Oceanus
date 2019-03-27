5c46c6ae01c95a3ddd5a1a66d5dba6499ecd89b6
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-27 12:16:48 -0400

    test status 200 if species is correct

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Route Handlers', () => {
-    describe('GET /', () => {
+    describe('GET /api/v1/animals', () => {
         it('responds with 200', async () => {
             const response = await request(app).get('/api/v1/animals');
 
@@ -15,4 +15,12 @@ describe('Route Handlers', () => {
             expect(typeof response.body).toBe('object');
         });
     });
+
+    describe('GET api/v1/animals/:species', () => {
+        it('responds with 200', async () => {
+            const response = await request(app).get('/api/v1/animals/brachiopods');
+
+            expect(response.status).toBe(200);
+        });
+    })
 });
