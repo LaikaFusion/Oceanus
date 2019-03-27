7a5ccf95be1a649f75f5072d189b6c24c1e0637a
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-27 13:11:05 -0400

    created get request for subspecies

--- api/routes/animals.js
+++ api/routes/animals.js
@@ -22,4 +22,20 @@ router.get('/:species', (req, res, next) => {
     }
 });
 
+router.get('/:species/:subspecies', (req, res) => {
+    const species = req.params.species;
+    const subspecies = req.params.subspecies
+
+    if(animals[species][subspecies] === undefined) {
+        res.status(404).json({
+            status: '404: Not Found'
+        });
+    } else {
+        res.status(200).json({
+            status: animals.status,
+            species: animals[species][subspecies]
+        });
+    }
+});
+
 module.exports = router;
