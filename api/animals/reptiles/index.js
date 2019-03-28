92c018d5d836106e2e49809d8f8e70e4151ba488
LaikaFusion <newrad4@gmail.com>
2019-03-28 15:58:42 -0400

    fixes to reptile file

--- api/animals/reptiles/index.js
+++ api/animals/reptiles/index.js
@@ -2,7 +2,7 @@
 const animals = {
     greenTurtle: {
         id: 1,
-        name: `Green turtle (Actinia equina)`,
+        name: `Green turtle (Chelonia mydas)`,
         description: `Animal covered by a heavy shell that consists of an upper and lower part. Upper part with central, four costal and bordering plates. The first costal plate does not touch the nuchal. Shell brown to dark brown. Shell plates lie side by side, not overlapping. Head with two pairs of plates between the eyes and four plates behind the eye.`,
         size: `They are large, usually 1.5 m in length and a mass of 205 kg, on average.`,
         colouration: `Shell brown to dark brown.`,
@@ -18,7 +18,7 @@ const animals = {
         colouration: `Shell yellow-brown with fan-like markings`,
         habitat: `The Hawksbill turtle is omnivorous, feeding both on plant and animal material. They spent their lives mostly in the water. Only females come ashore to nest; males rarely return to land after crawling into the sea as hatchlings. Most females return to nest on the beach where they were born. A female will not nest in consecutive years, typically skipping one or two years before returning.`,
         distribution: `Common Caribbean.`,
-        img: `${process.env.BASE_ROUTE}/img/seaAnemones/img/beadletAnemone/beadlet-anemone.jpg`
+        img: `${process.env.BASE_ROUTE}/img/reptiles/img/hawksbillTurtle/hawksbill-turtle.jpg`
     },
 };
 
